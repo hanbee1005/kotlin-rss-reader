291d888f90de549c9a545c16223689af8fee5924
hanbee son <hbson@hyundai.com>
2025-04-21 21:36:42 +0900

    feat: rss 데이터 정렬 후 최대 10개 출력 제한

--- src/main/kotlin/rss/controller/RssController.kt
+++ src/main/kotlin/rss/controller/RssController.kt
@@ -2,11 +2,13 @@ package rss.controller
 
 import rss.model.Post
 import rss.service.WoowahanPostService
+import kotlin.math.min
 
 class RssController(
     val woowahanPostService: WoowahanPostService,
 ) {
     fun getPosts(): List<Post> {
-        return woowahanPostService.getPosts()
+        val woowahanPosts = woowahanPostService.getPosts().sortedByDescending { it.pubDate }
+        return woowahanPosts.take(min(10, woowahanPosts.size))
     }
 }
